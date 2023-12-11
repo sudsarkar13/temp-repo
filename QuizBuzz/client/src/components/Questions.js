99060a560c8d9fa85af829ede1c1573f71bdd6b2
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-12-12 00:22:20 +0530

    update

--- QuizBuzz/client/src/components/Questions.js
+++ QuizBuzz/client/src/components/Questions.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Questions() {
+
+    const [ checked, setchecked ] = useState(undefined)
+
+    function onSelect() {
+        setchecked(true)
+        console.log('radio button change')
+    }
+
   return (
     <div className='questions'>
         <h2 className='text-light'>Simple Question 1</h2>
@@ -8,7 +16,7 @@ export default function Questions() {
         {/* Display options */}
         <ul>
             <li>
-                <input type='radio' name='options' value={true} />
+                <input type='radio' name='options' value={checked} id='q1-option' onChange={onSelect} />
                 <label>Option 1</label>
             </li>
         </ul>
--- QuizBuzz/client/src/components/Quiz.js
+++ QuizBuzz/client/src/components/Quiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Main.css';
 import Questions from './Questions';
 
 
