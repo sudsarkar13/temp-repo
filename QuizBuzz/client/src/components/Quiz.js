808f5aa9b8dbebc7e259f18b76160ee33818893f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-12-11 23:26:08 +0530

    Updates

--- QuizBuzz/client/src/components/Quiz.js
+++ QuizBuzz/client/src/components/Quiz.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 
 export default function Quiz() {
+
+  // Previous button event handler
+  function onPrev() {
+    console.log('On prev click')
+  }
+  // Next button event handler
+  function onNext() {
+    console.log('On next click')
+  }
+
   return (
     <div className='container'>
       <h1 className='title text-light'>QuizBuzz</h1>
+
+      {/* Display Questions */}
+
+      <div className='grid'>
+        <button className='btn prev' onClick={onPrev}>Previous</button>
+        <button className='btn next' onClick={onNext}>Next</button>
+      </div>
     </div>
   )
 }
