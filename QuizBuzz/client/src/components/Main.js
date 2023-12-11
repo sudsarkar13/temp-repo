591e2b2032fb620cdcb68c7108b808a52b3344b7
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-12-11 15:12:23 +0530

    Update Main.js

--- QuizBuzz/client/src/components/Main.js
+++ QuizBuzz/client/src/components/Main.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
+
+  const inputRef = useRef(null)
+
   return (
     <div className='container'>
-        <h1 className='title text-light'>QuizBuzz</h1>
+      <h1 className='title text-light'>QuizBuzz</h1>
 
-        <ol>
-          <li className='text-light'>Click on the "Start Quiz" button to begin.</li>
-          <li className='text-light'>You will be presented with a series of questions.</li>
-          <li className='text-light'>Select the answer you think is correct.</li>
-          <li className='text-light'>At the end of the quiz, you will be presented with your score.</li>
-        </ol>
+      <ol>
+        <li className='text-light'>Click on the "Start Quiz" button to start the quiz.</li>
+        <li className='text-light'>You will be presented with a series of multiple-choice questions.</li>
+        <li className='text-light'>Select the answer you think is correct by clicking on the radio button.</li>
+        <li className='text-light'>Click on the "Next" button to go to the next question.</li>
+        <li className='text-light'>Click on the "Previous" button to go to the previous question.</li>
+        <li className='text-light'>Click on the "Submit" button to submit your answers.</li>
+        <li className='text-light'>Click on the "View Results" button to view your results.</li>
+      </ol>
+
+      <form id='form'>
+        <input ref={inputRef} type='text' placeholder='Username*' required></input>
+      </form>
+
+      <div className='start'>
+        <Link className='btn' to='/quiz'>Start Quiz</Link>
+      </div>
     </div>
   )
 }
