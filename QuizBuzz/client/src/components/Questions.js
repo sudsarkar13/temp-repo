6c9837839478eb44ac6951255e72432b3eb604ed
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2023-12-16 06:51:49 +0530

    Update and fixed issues
    Fixed issues of data.js 'export default'
    
    Updated Questions.js

--- QuizBuzz/client/src/components/Questions.js
+++ QuizBuzz/client/src/components/Questions.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import data from '../database/data'
 
 export default function Questions() {
 
-    const [ checked, setChecked ] = useState(undefined)
+    const [checked, setChecked] = useState(undefined)
+
+    const question = data[0]
+
+    useEffect(() => {
+        console.log(question)
+    })
 
     function onSelect() {
         setChecked(false)
         console.log('radio button change')
     }
 
-  return (
-    <div className='questions'>
-        <h2 className='text-light'>Simple Question 1</h2>
+    return (
+        <div className='questions'>
+            <h2 className='text-light'>{question.question}</h2>
 
-        {/* Display options */}
-        <ul>
-            <li>
-                <input type='radio' name='options' value={checked} id='q1-option' onChange={onSelect} />
-                <label className='text-primary' htmlFor='q1-option'>Option </label>
-                <div className='check'></div>
-            </li>
-        </ul>
-    </div>
-  )
+            {/* Display options */}
+            <ul key={question.id}>
+                {
+                    question.options.map((q, i) => (
+                        <li key={i}>
+                            <input type='radio' name='options' value={checked} id={`q${i}-option`} onChange={onSelect} />
+                            <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+                            <div className='check'></div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
