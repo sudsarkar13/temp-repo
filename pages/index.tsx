24fa4ea354b8b0908cb4cd1c522222405eb783f5
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-01 23:40:11 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,6 @@ const HomePage = () => {
 		<div>
 			<Head>
 				<title>Maa Tarini Electrical</title>
-				<meta
-					name='description'
-					content='Electrical Shop & Repairing Services'
-				/>
-				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
 		</div>
