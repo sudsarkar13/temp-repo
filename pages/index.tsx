7377e303057c1809ad9164aefdab16bf3e4bd877
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-17 03:11:59 +0530

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,10 @@ const HomePage = () => {
 		<div className='overflow-x-hidde'>
 			<Head>
 				<title>Maa Tarini Electrical</title>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="description" content="Maa Tarini Electrical is an electrical shop based in Cuttack, Odisha. We offer complete range of electrical services." />
+				<meta
+					name='description'
+					content='Maa Tarini Electrical is an electrical shop based in Cuttack, Odisha. We offer complete range of electrical services.'
+				/>
 			</Head>
 			<div>
 				<MobileNav nav={nav} closeNav={closeNav} />
