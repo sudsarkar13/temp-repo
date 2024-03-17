4ee60980aa8b9c12fb26913ab364189bb706d2cb
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-18 00:55:15 +0530

    Update

--- components/hero/gallery/Partners.tsx
+++ components/hero/gallery/Partners.tsx
@@ -6,13 +6,18 @@ const Partners = () => {
 		<div className='partners'>
 			<h1>Our trusted electrical partners</h1>
 			<div className='partners-logo'>
-				<img src="./websiteImages/companyLogos/ABBLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/AnchorPanasonicLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/BajajElectricalsLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/ConaLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/CromptonGreavesLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/CSElectricLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/FinolexCablesLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/GoldmedalElectricalsLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/HavellsLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/OrientElectricLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/PanasonicLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/PhilipsLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/PMConaLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/PolycabWiresLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/RRKabelLogo.png" alt='Brands' />
 				<img src="./websiteImages/companyLogos/VguardLogo.png" alt='Brands' />
