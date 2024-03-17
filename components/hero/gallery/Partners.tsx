e7f5991587b92549b7755630eb2d990bb7beee38
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-17 23:36:40 +0530

    Update

--- components/hero/gallery/Partners.tsx
+++ components/hero/gallery/Partners.tsx
@@ -1,7 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 const Partners = () => {
-	return <div className="partners">Partners</div>;
+	return (
+		<div className='partners'>
+			<h1>Our trusted electrical partners</h1>
+			<div className='partners-logo'>
+				<img src="./websiteImages/companyLogos/ABBLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/BajajElectricalsLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/CromptonGreavesLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/FinolexCablesLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/HavellsLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/OrientElectricLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/PhilipsLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/PolycabWiresLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/RRKabelLogo.png" alt='Brands' />
+				<img src="./websiteImages/companyLogos/VguardLogo.png" alt='Brands' />
+				{/* <img src="" alt='Brands' /> */}
+			</div>
+		</div>
+	);
 };
 
 export default Partners;
