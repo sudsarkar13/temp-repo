f43d49d744f35b54c1f2c43ad55501a5fd8fd805
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-03-22 01:26:43 +0530

    Update

--- test/components/hero/gallery/GalleryTest.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GalleryTest = () => {
-	return <div className="slider">GalleryTest</div>;
-};
-
-export default GalleryTest;
--- test/components/navigation/index.ts
+++
@@ -1,5 +0,0 @@
-import MobileNavTest from './MobileNavTest';
-// @/test/components/navigation/index.ts or @/test/components/navigation/index.tsx
-
-export { default as NavTest } from "./NavTest";
-export { default as MobileNavTest } from "./MobileNavTest";
