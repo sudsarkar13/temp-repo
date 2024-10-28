be74d9e052dff52cc2625dc4e2c387bdda409a69
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-28 14:30:54 +0530

    refactor ♻️: Reorganized tailwind config file styles constants
    - Refactored tailwind configuration file organinning styles constants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,15 @@ const config: Config = {
 		container: {
 			center: true,
 			padding: "15px",
-			screens: {
-				sm: "640px",
-				md: "768px",
-				lg: "960px",
-				xl: "1200px",
-			},
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		fontFamily: {
+			primary: "var(--font-jetbrains-mono)",
 		},
 		extend: {
 			colors: {
