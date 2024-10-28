4b7fb4467d7492e2fb80b22fa3c1b6c0dd9d3e35
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-28 20:33:05 +0530

    feat ✨: Added responsive navigation menus
    - Added responsive main and mobile navigation menus.

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui";
+import Nav from "./Nav";
 
 const Header: React.FC = () => {
 	return (
 		<main>
 			<header className={`py-8 xl:py-12 text-white`}>
-				<div className={`container mx-auto`}>
+				<div className={`container mx-auto flex justify-between`}>
 					{/* logo */}
 					<Link href={`/`}>
 						<h1 className={`text-4xl font-semibold`}>
 							Sudeepta <span className={`text-accent`}>.</span>
 						</h1>
 					</Link>
+					{/* desktop nav & hire me button */}
+					<div className={`hidden xl:flex items-center gap-8`}>
+						<Nav />
+						<Link href={`/contact`}>
+							<Button className={`bg-[#151518]`}>Hire me</Button>
+						</Link>
+					</div>
+
+          {/* mobile nav */}
+          <div className={`xl:hidden`}>mobile nav</div>
 				</div>
 			</header>
 		</main>
