9fb3e1f0c410dbf545f280c4b1bc2b0d25c1f03f
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-28 19:49:51 +0530

    feat ✨: Integrate header navigation component
    - Header navigation component integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono} from "next/font/google";
 import "./globals.css";
+import Header from "@/components/navigation/Header";
 
 const jetBrainsMono = JetBrains_Mono({
 	subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
 		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${jetBrainsMono.variable} antialiased`} suppressHydrationWarning>
+					<Header />
 				{children}
 			</body>
 		</html>
