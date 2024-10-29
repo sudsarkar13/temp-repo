f3c4b99a65461cd218e5b3daaafc491b5c1d58e9
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-29 14:56:55 +0530

    style 💎: Adjusted page transition animations for accessibility and smooth fast transitions.
    - Converted page transition's animation duration to faster timing to comply accessibility practices then has shifted slight slower effects providing still same animation impact and better practice compliant transition periods across effects from slightly smooth rapid to still same calm normal velocity of new page loaded response over first then latter views perspective duration behavior was overall altered has added full an additional of the default rapid  milliseconds were updated for page transition animations, slower animated fade animation shifted over originally found data quicker seconds prior to fully delay whole one final values were also updated originally over exactly being an amount needed single decimal three. S
    
    main output a quicker seconds
    
    Modified timeout times animated responses made adjustments are longer response will now reflect animations timing edits for more accurately desired smooth fast transitions

--- components/page/PageTransition.tsx
+++ components/page/PageTransition.tsx
@@ -16,7 +16,7 @@ const PageTransition: React.FC<{ children: React.ReactNode }> = ({
 						initial={{ opacity: 1 }}
 						animate={{
 							opacity: 0,
-							transition: { delay: 0.5, duration: 0.2, ease: "easeInOut" },
+							transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
 						}}
 						className={`h-screen w-screen fixed bg-primary top-0 pointer-events-none`}
 					/>
