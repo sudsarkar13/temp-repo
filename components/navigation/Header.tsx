16be86f5689958745a6f66924cd68e2dc2b2fae7
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-30 07:37:14 +0530

    style 💎: Remove unnecessary spacing
    - Removed the sapce between the Sudeepta and the fullstop notation.

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
 					{/* logo */}
 					<Link href={`/`}>
 						<h1 className={`text-4xl font-semibold`}>
-							Sudeepta <span className={`text-accent`}>.</span>
+							Sudeepta<span className={`text-accent`}>.</span>
 						</h1>
 					</Link>
 					{/* desktop nav & hire me button */}
