eb0fbf7b71c3434e2aa22f5bbb463c1145307652
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-31 10:39:12 +0530

    style 💎: Simplified and unified visual elements and code
    - Unified whitespace and other visually-suggested typos have been tweaked against code proper that got implemented while blinking effect set steps removed from given types available in style classes although the real deal mainly includes major simplifications towards several specific styled animation within less room from our Tailwind set applied.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,10 +24,10 @@ const config: Config = {
 		},
 		extend: {
 			colors: {
-				primary: "#1c1c22",
+				primary: '#1c1c22',
 				accent: {
-					DEFAULT: "#00ff99",
-					hover: "#00e187",
+					DEFAULT: '#00ff99',
+					hover: '#00e187',
 				},
 			},
 			borderRadius: {
@@ -52,35 +52,10 @@ const config: Config = {
 						height: "0",
 					},
 				},
-				typewriter: {
-					to: {
-						left: "100%",
-					},
-				},
-				blink: {
-					"0%": {
-						opacity: "0",
-					},
-					"0.1%": {
-						opacity: "1",
-					},
-					"50%": {
-						opacity: "1",
-					},
-					"50.1%": {
-						opacity: "0",
-					},
-					"100%": {
-						opacity: "0",
-					},
-				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
-				typewriter: "typewriter 2s steps(11) forwards",
-				caret:
-					"typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s",
 			},
 		},
 	},
