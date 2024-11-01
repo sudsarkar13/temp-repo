092e8b47f5142c2bf20789d8824d9c2eefb777e5
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-01 23:55:38 +0530

    refactor ♻️: Structured stats component UI for better organization
    - Refactored stats component UI structure for better organization and readability.

--- components/stats/Stats.tsx
+++ components/stats/Stats.tsx
@@ -23,7 +23,7 @@ const Stats: React.FC = () => {
 				joinedYear: stats.joinedYear,
 			});
 		} catch (error) {
-			console.error("Error fetching GitHub stats:", error);
+			// console.error("Error fetching GitHub stats:", error);
 		} finally {
 			setIsLoading(false);
 		}
@@ -59,45 +59,53 @@ const Stats: React.FC = () => {
 	];
 
 	return (
-		<main className={`container mx-auto`}>
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{
-					opacity: 1,
-					transition: { delay: 2, duration: 0.4, ease: "easeIn" },
-				}}
-				className={`flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none pb-4`}>
-				{stats.map((stat, index) => {
-					return (
-						<motion.div
-							initial={{ opacity: 0 }}
-							animate={{
-								opacity: 1,
-								transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
-							}}
-							key={index}
-							className={`flex-1 flex gap-4 items-center justify-center xl:justify-start`}>
-							<div className="relative">
-								<CountUp
-									end={stat.num}
-									duration={2}
-									delay={0}
-									className={`text-4xl xl:text-6xl font-extrabold`}
-								/>
-								{stat.loading && (
-									<span className="absolute top-4 md:-top-4 -right-28 md:-right-24 w-2 h-2 bg-green-500 rounded-full animate-pulse" />
-								)}
-							</div>
-							<p
-								className={`${
-									stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-								} leading-snug text-white/80`}>
-								{stat.text}
-							</p>
-						</motion.div>
-					);
-				})}
-			</motion.div>
+		<main>
+			<section className={`pt-4 pb-12 xl:pt-0 xl:pb-0`}>
+				<div className={`container mx-auto`}>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+							transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+						}}
+						className={`flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none pb-4`}>
+						{stats.map((stat, index) => {
+							return (
+								<motion.div
+									initial={{ opacity: 0 }}
+									animate={{
+										opacity: 1,
+										transition: {
+											delay: 2.4,
+											duration: 0.4,
+											ease: "easeInOut",
+										},
+									}}
+									key={index}
+									className={`flex-1 flex gap-4 items-center justify-center xl:justify-start`}>
+									<div className="relative">
+										<CountUp
+											end={stat.num}
+											duration={2}
+											delay={0}
+											className={`text-4xl xl:text-6xl font-extrabold`}
+										/>
+										{stat.loading && (
+											<span className="absolute top-4 md:-top-4 -right-28 md:-right-24 w-2 h-2 bg-green-500 rounded-full animate-pulse" />
+										)}
+									</div>
+									<p
+										className={`${
+											stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+										} leading-snug text-white/80`}>
+										{stat.text}
+									</p>
+								</motion.div>
+							);
+						})}
+					</motion.div>
+				</div>
+			</section>
 		</main>
 	);
 };
