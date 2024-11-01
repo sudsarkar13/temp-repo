5b70a01bbfdc3911f27b93f930dabe7be7103113
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-01 21:28:37 +0530

    style 💎: Streamlined stats card styles
    - Streamlined stats card resize feature styles, size labels.

--- components/photo/Photo.tsx
+++ components/photo/Photo.tsx
@@ -117,16 +117,16 @@ const Photo: React.FC = () => {
 						opacity: 1,
 						transition: { delay: 2, duration: 0.4, ease: "easeIn" },
 					}}
-					className="absolute bottom-4 right-0 p-3 sm:p-4 rounded-xl backdrop-blur-sm bg-black/20 
+					className="absolute -bottom-2 sm:bottom-2 md:bottom-0 -right-6 md:-right-24 p-2 sm:p-4 rounded-xl backdrop-blur-sm bg-black/20 
 						shadow-[0_8px_30px_rgb(0,0,0,0.12)] border border-white/10 
-						w-[280px] sm:w-[380px] md:w-[400px] hidden md:block">
+						w-[240px] sm:w-[380px] md:w-[400px]">
 					<motion.div
 						initial={{ opacity: 0 }}
 						animate={{
 							opacity: 1,
 							transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
 						}}
-						className="flex flex-col gap-2 sm:gap-3">
+						className="flex flex-col gap-1.5 sm:gap-3">
 						<div className="flex items-center mb-1 sm:mb-2">
 							<Github className="w-4 h-4 sm:w-5 sm:h-5 mr-2 text-white/70" />
 						</div>
@@ -136,17 +136,17 @@ const Photo: React.FC = () => {
 									key={index}
 									className="flex items-center">
 									<div className="w-full">
-										<div className="flex flex-row gap-4 sm:gap-6 items-center justify-between">
+										<div className="flex flex-row gap-1.5 sm:gap-6 items-center justify-between">
 											<CountUp
 												end={stat.num}
 												duration={2}
 												delay={0}
-												className="text-3xl sm:text-4xl xl:text-6xl font-extrabold text-white/90"
+												className="text-2xl sm:text-4xl xl:text-6xl font-extrabold text-white/90"
 											/>
 											{stat.loading && (
 												<span className="absolute top-[12px] right-2 w-2 h-2 bg-green-500 rounded-full animate-pulse" />
 											)}
-											<p className="text-white/70 flex-1 text-right text-sm sm:text-base">
+											<p className="text-white/70 flex-1 text-right text-xs sm:text-base">
 												{stat.text}
 											</p>
 										</div>
