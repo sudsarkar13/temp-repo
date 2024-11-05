d8ab45b89344464f64d941ad69624693a3e22a5c
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-05 12:31:03 +0530

    style 💎: Enhance service cards transitions and design
    - Service cards receive a refresh enhancements including background and text on hover transitional effects additions primarily for their icons numbers and titles while descriptions have been subtly updated.

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -56,19 +56,19 @@ const ServicesPage: React.FC = () => {
 									className={`flex-1 flex flex-col justify-center gap-6 group`}>
 									{/* top */}
 									<div className={`w-full flex justify-between items-center`}>
-										<div className={`text-5xl font-extrabold text-outline text-transparent`}>
+										<div className={`text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500`}>
 											{service.num}
 										</div>
-										<Link href={service.href}>
-											<ArrowDownRight />
+										<Link href={service.href} className={`w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45`}>
+											<ArrowDownRight className={`text-primary text-3xl`} />
 										</Link>
 									</div>
-									{/* heading */}
-									<h2 className={`text-4xl font-semibold capitalize`}>
+									{/* title */}
+									<h2 className={`text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500`}>
 										{service.title}
 									</h2>
 									{/* description */}
-									<p>{service.description}</p>
+									<p className={`text-white/60`}>{service.description}</p>
 									{/* border */}
 									<div className={`border-b border-white/20 w-full`}></div>
 								</div>
