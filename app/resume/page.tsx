0040f803aa0155797048eb9c2187ed919139a5a5
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-06 21:15:56 +0530

    refactor ♻️: Improve resume structure and formatting
    - Renewed resume with personal statement paragraphs formatted description sections added latest additional project milestones duration edits adding reiteration showcase of advanced work and upgraded form descriptions fields display čtyImprove resume added form restructured personal display entries

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -92,9 +92,18 @@ const experience = {
 	items: [
 		{
 			company: "Healthunity Solutions Pvt Ltd",
-			position: "CTO",
-			role: "Frontend Developer Lead",
-			duration: "2024 - Present",
+			position: "CTO & Frontend Developer Lead",
+			duration: "Mar 2024 - Present",
+		},
+		{
+			company: "ProU Education",
+			position: "Campus Ambassador KIIT University",
+			duration: "Apr 2023 - Nov 2023",
+		},
+		{
+			company: "Freelance",
+			position: "System Engineer",
+			duration: "May 2018 - Apr 2023",
 		},
 	],
 };
@@ -190,13 +199,44 @@ const ResumePage: React.FC = () => {
 						{/* content */}
 						<div className={`min-h-[70vh] w-full`}>
 							{/* About */}
-							<TabsContent value="about" className={`w-full`}>About Me</TabsContent>
+							<TabsContent value="about" className={`w-full`}>
+								About Me
+							</TabsContent>
 							{/* Education */}
-							<TabsContent value="education" className={`w-full`}>Education</TabsContent>
+							<TabsContent value="education" className={`w-full`}>
+								Education
+							</TabsContent>
 							{/* Experience */}
-							<TabsContent value="experience" className={`w-full`}>Experience</TabsContent>
+							<TabsContent value="experience" className={`w-full`}>
+								<div
+									className={`flex flex-col gap-[30px] text-center xl:text-left`}>
+									<h3 className={`text-4xl font-bold`}>{experience.title}</h3>
+									<p className={`max-w-[600px] text-white/60 mx-auto xl:mx-0 `}>
+										{experience.description}
+									</p>
+									<ScrollArea className={`h-[400px]`}>
+										<ul
+											className={`grid grid-cols-1 lg:grid-cols-2 gap-[30px]`}>
+											{experience.items.map((item, index) => {
+												return (
+													<li key={index}>
+														<span>{item.duration}</span>
+														<h3>{item.position}</h3>
+														<div>
+															<span></span>
+															<p>{item.company}</p>
+														</div>
+													</li>
+												);
+											})}
+										</ul>
+									</ScrollArea>
+								</div>
+							</TabsContent>
 							{/* Skills */}
-							<TabsContent value="skills" className={`w-full`}>Skills</TabsContent>
+							<TabsContent value="skills" className={`w-full`}>
+								Skills
+							</TabsContent>
 						</div>
 					</Tabs>
 				</div>
