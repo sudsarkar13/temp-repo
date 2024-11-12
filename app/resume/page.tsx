5f483a14196587c69baa47fba9baca3e0c9d0fae
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-13 00:31:50 +0530

    feat ✨: Update resume skills section with redesign
    - Updated resume skills section with redesign and addition of skill popup tool tip display and formatting changes for single skill summaries making room various improvements, design styling.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -299,17 +299,38 @@ const ResumePage: React.FC = () => {
 							{/* Skills */}
 							<TabsContent value="skills" className={`w-full h-full`}>
 								<div className={`flex flex-col gap-[30px]`}>
-									<div>
+									<div
+										className={`flex flex-col gap-[30px] text-center xl:text-left`}>
 										<div
 											className={`flex flex-col lg:flex-row items-center gap-8 text-4xl`}>
 											<span className={`text-accent`}>{skills.icon}</span>
-											<h3>{skills.title}</h3>
+											<h3 className={`text-4xl font-bold`}>{skills.title}</h3>
 										</div>
-										<p>{skills.description}</p>
+										<p
+											className={`max-w-[600px] text-white/60 mx-auto xl:mx-0`}>
+											{skills.description}
+										</p>
 									</div>
-									<ul>
+									<ul
+										className={`grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]`}>
 										{skills.skillList.map((skill, index) => {
-											return <li key={index}></li>;
+											return (
+												<li key={index}>
+													<TooltipProvider delayDuration={100}>
+														<Tooltip>
+															<TooltipTrigger className={`w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group`}>
+																<div
+																	className={`text-6xl group-hover:text-accent transition-all duration-300`}>
+																	{skill.icon}
+																</div>
+															</TooltipTrigger>
+															<TooltipContent>
+																<p className={``}>{skill.name}</p>
+															</TooltipContent>
+														</Tooltip>
+													</TooltipProvider>
+												</li>
+											);
 										})}
 									</ul>
 								</div>
