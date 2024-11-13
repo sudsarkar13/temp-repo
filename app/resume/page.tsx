f806b768195d6ac4b8ac7b46a48a25f2c730b336
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-13 11:33:02 +0530

    refactor ♻️: Refactored user details and about info
    - Refactor about information section and update fields about user address and professional experience in details.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -63,12 +63,16 @@ const about = {
 			fieldValue: "+91 7504614781",
 		},
 		{
-			fieldName: "Address:",
-			fieldValue: "P.C. Sarkar Lane, Arunodaya Nagar, Cuttack, Odisha, India",
-		},
-		{
 			fieldName: "Experience:",
 			fieldValue: calculateExperience(),
+		},
+		{
+			fieldName: "Company:",
+			fieldValue: "Healthunity Solutions Pvt Ltd",
+		},
+		{
+			fieldName: "Designation:",
+			fieldValue: "CTO",
 		},
 		{
 			fieldName: "Nationality:",
@@ -224,21 +228,23 @@ const ResumePage: React.FC = () => {
 									<p className={`max-w-[600px] text-white/60 mx-auto xl:mx-0`}>
 										{about.description}
 									</p>
-									<ScrollArea className={`h-[480px]`}>
-										<ul
-											className={`grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0`}>
-											{about.info.map((item, index) => {
-												return (
-													<li
-														key={index}
-														className={`flex items-center justify-center xl:justify-start gap-4`}>
-														<span className={`text-white/60`}>{item.fieldName}</span>
-														<span className={`text-md`}>{item.fieldValue}</span>
-													</li>
-												);
-											})}
-										</ul>
-									</ScrollArea>
+									<ul
+										className={`grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0`}>
+										{about.info.map((item, index) => {
+											return (
+												<li
+													key={index}
+													className={`flex items-center justify-center xl:justify-start gap-4`}>
+													<span className={`text-white/60`}>
+														{item.fieldName}
+													</span>
+													<span className={`text-md leading-snug`}>
+														{item.fieldValue}
+													</span>
+												</li>
+											);
+										})}
+									</ul>
 								</div>
 							</TabsContent>
 							{/* Education */}
