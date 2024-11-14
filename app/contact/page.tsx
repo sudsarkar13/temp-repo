468b95f331052c3f158ade5aab23abaa66315513
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-14 23:02:08 +0530

    feat ✨: Add embedded SVG icons and customer assistance materials to contacts page
    - Enhanced contacts page with embedded SVG icons and a template comprising Email and physical address labels showcasing initial customer assistance materials in HTML and Typese or JavaScript through application improvements code for future performance compatibility fix support.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import { Button, Input, Textarea } from "@/components/ui";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectLabel,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const iconStyles = `text-[20px] text-accent`;
+
+const info = [
+	{
+		icon: <FaPhoneAlt className={`${iconStyles}`} />,
+		title: "Phone",
+		description: "+91 7504614781, +91 7809518625",
+	},
+	{
+		icon: <FaEnvelope className={`${iconStyles}`} />,
+		title: "Email",
+		description: "admin@sudeeptasarkar.in, sudsarkar13@gmail.com",
+	},
+	{
+		icon: <FaMapMarkerAlt className={`${iconStyles}`} />,
+		title: "Address",
+		description:
+			"Plot No.178, DDN - 061, P.C. Sarkar Lane, Arunodaya Nagar, Cuttack, Odisha, Pincode - 753012",
+	},
+];
 
 const ContactPage: React.FC = () => {
-  return (
-    <main>
-      <div>Contact Page</div>
-    </main>
-  );
+	return (
+		<main>
+			<div>Contact Page</div>
+		</main>
+	);
 };
 
 export default ContactPage;
