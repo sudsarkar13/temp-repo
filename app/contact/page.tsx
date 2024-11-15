1f300544b0a304a06f78dc3fab2e89bc11c71df8
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-15 22:55:22 +0530

    feat ✨: Enhance user contact form with rich details and styling
    - Enhanced user contact form with style formatting simplification, a notification or details modal with richer detail's labels labels for email, phone description styling as full interface & design feature incorporation and grid definition input. Formatted well style interface has several function service now in component's screen description.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,21 +14,19 @@ import {
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-const iconStyles = `text-[20px] text-accent`;
-
 const info = [
 	{
-		icon: <FaPhoneAlt className={`${iconStyles}`} />,
+		icon: <FaPhoneAlt />,
 		title: "Phone",
 		description: "+91 7504614781, +91 7809518625",
 	},
 	{
-		icon: <FaEnvelope className={`${iconStyles}`} />,
+		icon: <FaEnvelope />,
 		title: "Email",
 		description: "admin@sudeeptasarkar.in, sudsarkar13@gmail.com",
 	},
 	{
-		icon: <FaMapMarkerAlt className={`${iconStyles}`} />,
+		icon: <FaMapMarkerAlt />,
 		title: "Address",
 		description:
 			"Plot No.178, DDN - 061, P.C. Sarkar Lane, Arunodaya Nagar, Cuttack, Odisha, Pincode - 753012",
@@ -45,13 +43,81 @@ const ContactPage: React.FC = () => {
 			}}
 			className={`py-6`}>
 			<div className={`container mx-auto`}>
-				<div>
+				<div className={`flex flex-col xl:flex-row gap-[30px]`}>
 					{/* form */}
-					<div>
-						<form>form</form>
+					<div className={`xl:w-[54%] order-2 xl:order-none`}>
+						<form
+							className={`flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl`}>
+							<h3 className={`text-4xl text-accent`}>Let's Talk</h3>
+							<p className={`text-white/60`}>
+								Great things are never done by one person. They're done by a
+								team of people working together. Let's collaborate and bring
+								your vision to life. Whether you have a specific project in mind
+								or just want to explore possibilities, I'm here to help turn
+								your ideas into reality. Together, we can create something
+								extraordinary.
+							</p>
+							{/* input */}
+							<div className={`grid grid-cols-1 md:grid-cols-2 gap-6`}>
+								<Input type="firstname" placeholder="First Name" />
+								<Input type="lastname" placeholder="Last Name" />
+								<Input type="email" placeholder="Email Address" />
+								<Input type="phone" placeholder="Phone Number" />
+							</div>
+							{/* select */}
+							<Select>
+								<SelectTrigger className={`w-full `}>
+									<SelectValue placeholder={`Select a service`} />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										<SelectLabel>Select a service</SelectLabel>
+										<SelectItem value="web-development">
+											Web Development
+										</SelectItem>
+										<SelectItem value="uiux-design">UI/UX Design</SelectItem>
+										<SelectItem value="android-app-development">
+											Android App Development
+										</SelectItem>
+										<SelectItem value="seo">SEO</SelectItem>
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+							{/* textarea */}
+							<Textarea
+								placeholder="Type your message here."
+								className={`h-[200px]`}
+							/>
+							{/* button */}
+							<div className={`flex justify-center`}>
+								<Button size={`md`} type="submit" className={`max-w-40`}>
+									Send message
+								</Button>
+							</div>
+						</form>
 					</div>
 					{/* info */}
-					<div>info</div>
+					<div
+						className={`flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0`}>
+						<ul className={`flex flex-col gap-10`}>
+							{info.map((item, index) => {
+								return (
+									<li key={index} className={`flex items-center gap-6`}>
+										<div
+											className={`w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center`}>
+											<div className={`text-[28px]`}>{item.icon}</div>
+										</div>
+										<div className={`flex-1`}>
+											<p className={`text-white/60`}>{item.title}</p>
+											<h3 className={`text-xl flex flex-wrap`}>
+												{item.description}
+											</h3>
+										</div>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
 				</div>
 			</div>
 		</motion.section>
