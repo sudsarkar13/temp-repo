8f4058128df1261bb04c32868f87c4218d08d723
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-17 17:08:33 +0530

    style 💎: Updated route import due to update
    - updated route to correct import due to update or shuffle.

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function POST() {
   cookies().delete('admin_token');
