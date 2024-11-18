cf41d14a033040306f21236e86e2344ba405e32e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-18 11:11:18 +0530

    style 💎: Modernize project information presentation layout
    - Modernization of project information presentation layout.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,24 +1,7 @@
 import React from "react";
-import dynamic from "next/dynamic";
 import Loading from "@/components/loading/loading";
 import { Project } from "@/types/project";
-
-// Dynamically import client components with no SSR
-const ProjectSlider = dynamic(
-  () => import("@/components/work/ProjectSlider"),
-  { 
-    ssr: false,
-    loading: () => <Loading />
-  }
-);
-
-const ProjectDetails = dynamic(
-  () => import("@/components/work/ProjectDetails"),
-  {
-    ssr: false,
-    loading: () => <Loading />
-  }
-);
+import WorkContent from "@/components/work/WorkContent";
 
 // Fetch projects on the server
 async function getProjects(): Promise<Project[]> {
@@ -52,12 +35,7 @@ export default async function WorkPage() {
   return (
     <section className={`min-h-[80vh] flex flex-col justify-center py-12 xl:px-0`}>
       <div className={`container mx-auto`}>
-        <div className={`flex flex-col xl:flex-row gap-4 xl:gap-[30px]`}>
-          <ProjectDetails project={projects[0]} />
-          <div className={`w-full xl:w-[50%] order-1 xl:order-none`}>
-            <ProjectSlider projects={projects} />
-          </div>
-        </div>
+        <WorkContent projects={projects} />
       </div>
     </section>
   );
